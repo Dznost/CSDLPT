a6c17d6b7c782457ac8edfb4343391c79a0d74df
Dznost <bao1vip@gmail.com>
2025-12-01 23:05:37 +0700

    Clean project structure

--- WebQuanLySinhVien/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebQuanLySinhVien.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
